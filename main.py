74d6306985b1334eacce65abe010c23e39ec6f82
aidarminyazev <legodark@mail.ru>
2024-03-20 20:33:40 +0500

    ephemeral message

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from discord import app_commands
+
 
 load_dotenv()  # Loads the env variables from .env file
 TOKEN = os.getenv("DISCORD_BOT_SECRET")  # Fetching token from env variable
 
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='$', intents=intents)
+intents.message_content = True
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
 
-@bot.command(name="hello", help="Responds with a greeting")
-async def hello(ctx):
-    await ctx.send('Hello, {0.author.mention}'.format(ctx.message))
+
+# @bot.command(name="hello", help="Responds with a greeting")
+# async def hello(ctx):
+#     await ctx.send(f"Hello, {ctx.message.author.mention}")
+
+
+@tree.command(name="hello", description="Responds with a greeting")
+async def hello(interaction):
+    await interaction.response.send_message(
+        content="Секретное сообщение", ephemeral=True
+    )
+
 
 @bot.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(bot))
+    await tree.sync()
+    print(f"We have logged in as {bot.user}")
+
 
 if __name__ == "__main__":
     bot.run(TOKEN)
