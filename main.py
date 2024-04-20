88bc1ee7b17af3584a30e7c057d11a50722f428c
aidarminyazev <legodark@mail.ru>
2024-04-21 02:04:42 +0500

    added logger, loading extensions using cycle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -7,23 +8,33 @@ from discord.ext import commands
 intents = discord.Intents().all()
 intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
+commands_dir = "bot"
+logger = logging.getLogger("discord")
+
 
 @bot.command(name="ping1", help="Responds with pong")
 async def ping1(ctx):
     await ctx.send("pong")
 
+
 @bot.event
 async def on_ready():
-    await bot.load_extension("bot.test_commands")
-    await bot.load_extension("bot.event")
+    for filename in os.listdir(commands_dir):
+        if filename.endswith(".py") and filename != "__init__.py":
+            try:
+                await bot.load_extension(f"{commands_dir}.{filename[:-3]}")
+                logger.info(f"Loaded {filename}")
+            except Exception as e:
+                logger.error(f"Error loading {filename}: {e}")
     await bot.tree.sync()
-    print(f"We have logged in as {bot.user}")
+    logger.info(f"Logged in as {bot.user}")
 
 
 def main():
     load_dotenv()
     TOKEN = os.getenv("DISCORD_BOT_TOKEN")
     bot.run(TOKEN)
+
 
 if __name__ == "__main__":
     main()
