42b14e47040d5a32488a99f9d18662914de249a6
aidarminyazev <legodark@mail.ru>
2024-04-24 03:05:55 +0500

    intents changed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 
 
-intents = discord.Intents().all()
+intents = discord.Intents().default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 commands_dir = "app_commands"
