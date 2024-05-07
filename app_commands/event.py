47e82449a5dd9e3697c66017242024dbf4384469
Aidar Minyazev <legodark@mail.ru>
2024-05-08 01:19:11 +0500

    added commands for events history, fixes

--- app_commands/event.py
+++ app_commands/event.py
@@ -3,12 +3,16 @@ import discord
 from discord import app_commands
 from views.event_view import Event
 from modals.event_form import EventForm
+from views.history_view import HistoryView
+
 
 @app_commands.guild_only()
 class Commands(app_commands.Group):
     @app_commands.command(name="create_event", description="Создание нового события")
     async def create_event(self, interaction: discord.Interaction):
-        await interaction.response.send_modal(EventForm())
+        event_form = EventForm(interaction)
+        await interaction.response.send_modal(event_form)
+
 
     @app_commands.command(name="test_event", description="тест события")
     async def test_event(self, interaction: discord.Interaction):
@@ -20,8 +24,20 @@ class Commands(app_commands.Group):
             datetime.datetime.now() + datetime.timedelta(minutes=1),
             5,
         )
-        await event.send(interaction)
-# TODO: добавит команду просмотра истории событий
+        await event.send()
+
+    @app_commands.command(name="history_author", description="Созданные вами события")
+    async def history_author(self, interaction: discord.Interaction):
+        history_view = HistoryView(interaction.user.id, "автор")
+        await history_view.send(interaction)
+
+    @app_commands.command(
+        name="history_participant",
+        description="События, в которых вы принимали участие",
+    )
+    async def history_participant(self, interaction: discord.Interaction):
+        history_view = HistoryView(interaction.user.id, "участник")
+        await history_view.send(interaction)
 
 async def setup(bot):
     bot.tree.add_command(Commands(name="event", description="event commands"))
