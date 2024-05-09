f7e253c74a12fdb7d3ff6d3dd5a24bcc03b2f6e8
Aidar Minyazev <legodark@mail.ru>
2024-05-10 04:16:11 +0500

    changed db commands to receive a session

--- database/db.py
+++ database/db.py
@@ -27,22 +27,22 @@ async def get_session():
         await session.close()
 
 
-async def get_user_by_id(discord_id):
-    async with get_session() as session:
+async def get_user_by_id(session, discord_id):
+    async with session as session:
         user = await session.execute(select(User).where(User.discord_id == discord_id))
         return user.scalar_one_or_none()
 
 
-async def get_event_by_id(message_id):
-    async with get_session() as session:
+async def get_event_by_id(session, message_id):
+    async with session as session:
         event = await session.execute(
             select(Event).where(Event.message_id == message_id)
         )
         return event.scalar_one_or_none()
 
 
-async def get_events_by_author_id(discord_id, page=1, per_page=10):
-    async with get_session() as session:
+async def get_events_by_author_id(session, discord_id, page=1, per_page=10):
+    async with session as session:
         events = await session.execute(
             select(Event)
             .options(joinedload(Event.participants))
@@ -53,8 +53,8 @@ async def get_events_by_author_id(discord_id, page=1, per_page=10):
         )
         return events.scalars().unique().all()
 
-async def get_events_by_author_id_count(discord_id):
-    async with get_session() as session:
+async def get_events_by_author_id_count(session, discord_id):
+    async with session as session:
         events = await session.execute(
             select(func.count()).select_from(Event)
             .where(Event.author_id == discord_id)
@@ -62,8 +62,8 @@ async def get_events_by_author_id_count(discord_id):
         return events.scalar()
 
 
-async def get_events_by_participant_id(discord_id, page=1, per_page=10):
-    async with get_session() as session:
+async def get_events_by_participant_id(session, discord_id, page=1, per_page=10):
+    async with session as session:
         events = await session.execute(
             select(Event)
             .options(joinedload(Event.participants))
@@ -76,8 +76,8 @@ async def get_events_by_participant_id(discord_id, page=1, per_page=10):
         return events.scalars().unique().all()
 
 
-async def get_events_by_participant_id_count(discord_id):
-    async with get_session() as session:
+async def get_events_by_participant_id_count(session, discord_id):
+    async with session as session:
         events = await session.execute(
             select(func.count()).select_from(Event)
             .join(EventParticipants)
@@ -86,8 +86,8 @@ async def get_events_by_participant_id_count(discord_id):
         return events.scalar()
 
 
-async def get_participant(discord_id, event_id):
-    async with get_session() as session:
+async def get_participant(session, discord_id, event_id):
+    async with session as session:
         participant = await session.execute(
             select(EventParticipants).where(
                 EventParticipants.user_id == discord_id,
@@ -97,17 +97,17 @@ async def get_participant(discord_id, event_id):
         return participant.scalar_one_or_none()
 
 
-async def add_user(discord_id):
-    async with get_session() as session:
+async def add_user(session, discord_id):
+    async with session as session:
         new_user = User(discord_id=discord_id)
         session.add(new_user)
         await session.commit()
 
 
 async def add_event(
-    message_id, name, description, author_id, time, participants_needed
+    session, message_id, name, description, author_id, time, participants_needed
 ):
-    async with get_session() as session:
+    async with session as session:
         new_event = Event(
             message_id=message_id,
             name=name,
@@ -120,28 +120,24 @@ async def add_event(
         await session.commit()
 
 
-async def add_participant(participant_id, event_id):
-    async with get_session() as session:
+async def add_participant(session,  participant_id, event_id):
+    async with session as session:
         new_participant = EventParticipants(event_id=event_id, user_id=participant_id)
         session.add(new_participant)
         await session.commit()
 
 
-async def remove_event(message_id):
-    async with get_session() as session:
-        event = await get_event_by_id(message_id)
+async def remove_event(session, message_id):
+    async with session as session:
+        event = await get_event_by_id(session, message_id)
         if event:
             await session.delete(event)
             await session.commit()
 
 
-async def remove_participant(participant_id, event_id):
-    async with get_session() as session:
-        participant = await get_participant(participant_id, event_id)
+async def remove_participant(session, participant_id, event_id):
+    async with session as session:
+        participant = await get_participant(session, participant_id, event_id)
         if participant:
             await session.delete(participant)
             await session.commit()
-
-
-# async def setup(bot):
-#     await connect_to_db()
