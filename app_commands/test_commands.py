207b4c6c823feec9fb6385ce51ab074bd80e3103
aidarminyazev <legodark@mail.ru>
2024-05-24 21:36:13 +0500

    upd

--- app_commands/test_commands.py
+++ app_commands/test_commands.py
@@ -1,5 +1,8 @@
+import datetime
 import discord
 from discord import app_commands
+
+from views.event_view import Event
 
 class Commands(app_commands.Group):
 
@@ -10,6 +13,18 @@ class Commands(app_commands.Group):
     @app_commands.command(name="hello", description="Responds with a greeting")
     async def hello(self, interaction):
         await interaction.response.send_message(content=f"Привет, {interaction.user.mention}!", ephemeral=True)
+        
+    @app_commands.command(name="test_event", description="тест события")
+    async def test_event(self, interaction: discord.Interaction):
+        event = Event(
+            interaction,
+            interaction.user,
+            "Тестовое событие",
+            "Тестовое описание",
+            datetime.datetime.now() + datetime.timedelta(minutes=1),
+            5,
+        )
+        await event.send()
 
 
 async def setup(bot):
